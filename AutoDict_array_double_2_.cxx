c9a05833acadeea86d3f353a98217501b59f51a5
Alberto Perro <albertoperro.pse@gmail.com>
2019-12-15 15:42:17 +0100

    update readme

--- AutoDict_array_double_2_.cxx
+++
@@ -1,7 +0,0 @@
-#include "double,array"
-#ifdef __CINT__ 
-#pragma link C++ nestedclasses;
-#pragma link C++ nestedtypedefs;
-#pragma link C++ class array<double,2>+;
-#pragma link C++ class array<double,2>::*+;
-#endif
